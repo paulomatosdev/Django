4acbd998d018d157ffd57c2afee569da5d90b79a
Paulo matos <paulo.dematos23@gmail.com>
2026-09-22 16:17:02 -0300

    Adiciona modelos e registros administrativos para Question e Choice; implementa campo ativo em Question e cria migração correspondente.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
